b8f3455993ff61c1f7e8b03f991c81f654a2367b
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2024-04-16 14:52:30 +0100

    added footer and styling for footer and navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-      <div>
-        Here is Footer
-      </div>
-    );
-  }
+        <div className = "footer">
+          <span> Welcome to Our Shop! </span>
+          <span>Best quality! Perfect deals! </span>
+          
+           <nav>
+              <Link to="/">Home</Link>
+              <Link to="/Catalog">Find something for you</Link>
+              <Link to="/Blog"> Get to know more</Link>
+          </nav>
+        </div>
+      );
+    }
   
-  export default Footer;
+    export default Footer;
+  
+  
   
