4452e042f0b793d9a8d36415e025ac859e9c9f50
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2024-04-18 00:26:10 +0100

    few changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,11 @@ function Navbar() {
   return (
     <div>
       <nav className="navbar">
-        <NavLink to="/">Home</NavLink> | 
-        <NavLink to="/Basket">Basket</NavLink> | 
-        <NavLink to="/Blog"> Get to know More</NavLink> | 
-        <NavLink to="/Catalog">Find your Item</NavLink> | 
-        <NavLink to="/LogIn">Your account</NavLink> | 
+        <NavLink to="/">Home</NavLink> 
+        <NavLink to="/Basket">Basket</NavLink> 
+        <NavLink to="/Blog"> Get to know More</NavLink>
+        <NavLink to="/Catalog">Find your Item</NavLink>
+        <NavLink to="/LogIn">Your account</NavLink>
         <NavLink to="/ProductPage">About Item</NavLink>
       </nav>
     </div>
