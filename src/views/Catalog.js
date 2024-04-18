38e3706cb7ed4aa57801ff7f29f13450a446806b
NastasiaKarpenko <nastasia.karpenko@gmail.com>
2024-04-18 16:29:41 +0100

    navigation and routs between catalog and product page

--- src/views/Catalog.js
+++ src/views/Catalog.js
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom';
+const products = [
+    { id: '1', name: 'Product 1' },
+    { id: '2', name: 'Product 2' },
+   
+  ];
 
 function Catalog() {
+   
     return (
-      <div>
-        Here is Catalog
-      </div>
-    );
+            <div>
+              Here is Catalog
+              {products.map((product) => (
+                <div key={product.id}>
+                  {product.name}
+                  <Link to={`/catalog/${product.id}`} className="buttons">More</Link>
+                </div>
+              ))}
+            </div>
+          );
   }
   
   export default Catalog;
--- src/views/ProductPage.js
+++ src/views/ProductPage.js
@@ -1,8 +1,9 @@
-
+import { Link } from 'react-router-dom';
 function ProductPage() {
     return (
       <div>
         About Product
+        <Link to="/Catalog" className="buttons">Back to Catalog</Link>
       </div>
     );
   }
