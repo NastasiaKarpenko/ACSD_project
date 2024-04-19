687d6568f1603815ddb5856cbc124d36dc7d6ccc
lemosdaniel <daniel.alemos@hotmail.com>
2024-04-19 21:53:04 +0100

    ok

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <NavLink className="navbar-brand" to="/">Your Brand Name</NavLink>
+        <NavLink className="navbar-brand" to="/">Your Brand xName</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
